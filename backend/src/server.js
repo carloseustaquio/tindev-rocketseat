6e0c613f38d0e348779b30b84b72986ff5490c96
Carlos Eustáquio <caeu227@gmail.com>
2019-08-11 22:55:37 -0300

    First commit of TINDEV project

---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-cjr5z.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+
+server.use(routes);
+
+//GET, POST, PUT, DELETE
+
+
+
+server.listen(3333);
+
+// m = model, v - view, c - controler
