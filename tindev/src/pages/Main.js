c680cdee8411c7d4bb8a1c40909ccdb767dac28d
Carlos Eustáquio <caeu227@gmail.com>
2019-10-20 16:38:06 -0300

    adiciona demo no readme

--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function Main() {
-  return <View />
+  return(
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome Aboard!</Text>
+    </View>
+  ) 
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
+  },
+
+  text: {
+    color: '#DF4723',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+})
